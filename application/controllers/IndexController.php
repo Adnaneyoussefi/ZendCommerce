2b5b2a6b21e5eecae66dfbe807601b901ca77b3a
radid youssef <radid.yoyo@gmail.com>
2021-04-12 00:13:15 +0100

    ajouter modifier editer Categorie

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -14,21 +14,36 @@ class IndexController extends Zend_Controller_Action
 
     public function categorieAction()
     {
-        if(isset($_POST['nom'])){
-        $categorie = new Application_Model_Categorie();
-        $this->view->info = $categorie->addNewCategorie($_POST['nom']);
-        }
+        if(isset($_POST['nom']) && isset($_POST['id'])){
+            $categorie = new Application_Model_Categorie();
+            $this->view->info = $categorie->updateCategorie($_POST['id'],$_POST['nom']);
+            }
+        else if(isset($_POST['nom'])){
+            $categorie = new Application_Model_Categorie();
+            $this->view->info = $categorie->addNewCategorie($_POST['nom']);
+            }
+
         $categorie = new Application_Model_Categorie();
         $this->view->info = $categorie->getListCategories();
     }
 
     public function afficherAction()
     {
+        if(isset( $_GET['idS'] )){
+            $categorie = new Application_Model_Categorie();
+           $categorie->deleteCategorie($_GET['idS']);
+           header("Location: categorie");
+        }
+        else if(isset($_GET['idM'])){
+
+            $categorie = new Application_Model_Categorie();
+            $this->view->catModif = $categorie->getCategorieById($_GET['idM']);
+
+        }
     }
 
     public function modifierAction()
     {
-        // action body
     }
 
 

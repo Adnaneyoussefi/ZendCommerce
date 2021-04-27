aa5175a9ee2736cef82d6a1f0c95d447f5304831
Adnane Youssefi <adnaneyoussefi70@gmail.com>
2021-04-27 13:48:52 +0000

    Supprimer les constructeurs des modeles

--- application/models/Categorie.php
+++ application/models/Categorie.php
@@ -8,16 +8,6 @@ class Application_Model_Categorie
 
     private $produits = [];
 
-    private $bouchonne;
-
-    public function __construct()
-    {
-        $config = Zend_Controller_Front::getInstance()->getParam('bootstrap');
-        $apikey = $config->getOption('apikey');
-        $this->client = new Zend_Soap_Client($apikey);
-        $this->bouchonne = $config->getOption('bouchonne');
-    }
-
     public function getId()
     {
         return $this->id;
@@ -51,46 +41,5 @@ class Application_Model_Categorie
     {
         $this->produits[] = $produits;
         return $this;
-    }
-
-    public function getListCategories()
-    {
-        if ($this->bouchonne == 'on') {
-            $path_xml = APPLICATION_PATH . '/configs/getListCategories.xml';
-            return $this->convertResponseXML($path_xml);
-        } else {
-            return $this->client->getListCategories();
-        }
-
-    }
-
-    public function getCategorieById($id)
-    {
-        return $this->client->getCategorieById($id);
-    }
-
-    public function deleteCategorie($id)
-    {
-        return $this->client->deleteCategorie($id);
-    }
-
-    public function addNewCategorie($nom)
-    {
-        return $this->client->addNewCategorie($nom);
-    }
-
-    public function updateCategorie($id, $nom)
-    {
-        return $this->client->updateCategorie($id, $nom);
-    }
-
-    public function convertResponseXML($path_xml)
-    {
-        $xml = file_get_contents($path_xml);
-        //$xml = preg_replace('#[a-zA-Z0-9]+="[\#a-zA-Z0-9]+"#', '', $xml);
-        $xml = simplexml_load_string($xml);
-        $data = $xml->xpath("//SOAP-ENV:Body/*/*")[0];
-        $arrayResult = json_decode(json_encode($data));
-        return $arrayResult->item;
     }
 }
--- application/models/Produit.php
+++ application/models/Produit.php
@@ -18,16 +18,6 @@ class Application_Model_Produit
 
     private $client;
 
-    private $bouchonne;
-
-    public function __construct()
-    {
-        $config = Zend_Controller_Front::getInstance()->getParam('bootstrap');
-        $apikey = $config->getOption('apikey');
-        $this->client = new Zend_Soap_Client($apikey);
-        $this->bouchonne = $config->getOption('bouchonne');
-    }
-
     public function getId()
     {
         return $this->id;
@@ -103,64 +93,5 @@ class Application_Model_Produit
         $this->categorie = $categorie;
 
         return $this;
-    }
-
-    public function getListProduits()
-    {
-        //$client = $this->client;
-        $produits = [];
-        $categories = [];
-        if($this->bouchonne == 'on') {
-            $path_xml1 = APPLICATION_PATH . '/configs/getListProduits.xml';
-            $path_xml2 = APPLICATION_PATH . '/configs/getListCategories.xml';
-            $produits = $this->convertResponseXML($path_xml1);
-            $categories = $this->convertResponseXML($path_xml2);
-        }
-        else{
-            $produits = $this->client->getListProduits();
-            $categories = $this->client->getListCategories();
-        }
-        foreach ($produits as $p) {
-            foreach ($categories as $c) {
-                if ($p->categorie->id === $c->id) {
-                    $p->categorie = $c;
-                }
-            }
-        }
-        return $produits;
-    }
-
-    public function deleteProduit($id)
-    {
-        $client = $this->client;
-        return $this->client->deleteProduit($id);
-    }
-
-    public function getProduitById($id)
-    {
-        $client = $this->client;
-        return $this->client->getProduitById($id);
-    }
-
-    public function addNewProduit($nom, $description, $prix, $image, $quantite, $categorie_id)
-    {
-        $client = $this->client;
-        return $client->addNewProduit($nom, $description, $prix, $image, $quantite, $categorie_id);
-    }
-
-    public function updateProduit($id, $nom, $description, $prix, $image, $quantite, $categorie_id)
-    {
-        $client = $this->client;
-        return $client->updateProduit($id, $nom, $description, $prix, $image, $quantite, $categorie_id);
-    }
-
-    public function convertResponseXML($path_xml)
-    {
-        $xml = file_get_contents($path_xml);
-        //$xml = preg_replace('#[a-zA-Z0-9]+="[\#a-zA-Z0-9]+"#', '', $xml);
-        $xml = simplexml_load_string($xml);
-        $data = $xml->xpath("//SOAP-ENV:Body/*/*")[0];
-        $arrayResult = json_decode(json_encode($data));
-        return $arrayResult->item;
     }
 }
